3d015c8fd1e713881e62ffe21fd8aa657a1b3e09
Yurii Yaroshevskyi <oa9603@gmail.com>
2022-06-16 13:47:55 +0300

    added 699.py

---
+++ a = 699.py
@@ -0,0 +1,29 @@
+
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# print ("a + b", a + b)
+
+# # a = 30
+# # # b = 10
+# print("a-b", a-b)
+
+
+# # # a = 10
+# # # b = 5
+# print ("a*b", a*b)
+
+# # # a = 49
+# # # b = 7
+# print("a/b", a/b)
+
+# # a = 4
+# # b = 2
+
+user_name = input("What is your name?")
+print ("Your name is " + user_name, type(user_name))
+
+user_age = input("How old rgare you?")
+print("Your age is" + user_age, type(user_age))
+
+user_city = input("Where are you live?")
+print("You live in" + user_city, type(user_city))
